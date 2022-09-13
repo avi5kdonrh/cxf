2bc909fda62379f85b3aa30dbcba57ea28049693
Avinash Dongre <adongre@adongre.pnq.csb>
2022-09-13 18:46:07 +0530

    SOAPAction without quotes

--- rt/bindings/soap/src/test/java/org/apache/cxf/binding/soap/interceptor/SOAPActionWithoutQuotesTest.java
+++ rt/bindings/soap/src/test/java/org/apache/cxf/binding/soap/interceptor/SOAPActionWithoutQuotesTest.java
@@ -21,7 +21,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class SOAPActionWithoutQuotesTest {
     private IMocksControl control;
